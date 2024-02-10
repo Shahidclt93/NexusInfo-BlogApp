ba3937bd3acb011b2ee86877631bb7bc6e5f40cb
Syed Shameem <shahidclt93@gmail.com>
2024-02-11 00:29:53 +0530

    update

--- src/components/MetaData.jsx
+++ src/components/MetaData.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Helmet from "react-helmet";
+import logoTab from "../images/logo-tab.PNG";
 
 const MetaData = ({ title }) => {
   return (
     <Helmet>
       <title>{title}</title>
+      <link rel="icon" type="image/png" href={logoTab} />
     </Helmet>
   );
 };
