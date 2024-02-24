604eb8828ccb87c047870fec5ba03a9cd27dd2a9
Syed Shameem <shahidclt93@gmail.com>
2024-02-25 00:14:14 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React ,{useEffect} from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -19,7 +19,6 @@ import DeletePost from "./pages/DeletePost";
 import Logout from "./pages/Logout";
 import WebFont from "webfontloader";
 
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -28,37 +27,29 @@ const router = createBrowserRouter([
 
     children: [
       { index: true, element: <Home /> },
-      { key:"1", path: "posts/:id", element: <PostDetail /> },
-      { key:"2", path: "register", element: <Register /> },
-      { key:"3", path: "login", element: <Login /> },
-      { key:"4", path: "profile/:id", element: <UserProfile /> },
-      { key:"5", path: "authors", element: <Authors /> },
-      { key:"6", path: "create", element: <CreatePost /> },
-      { key:"7", path: "posts/categories/:category", element: <CategoryPosts /> },
-      { key:"8", path: "posts/users/:id", element: <AuthorPosts /> },
-      { key:"9", path: "myposts/:id", element: <Dashboard /> },
-      { key:"10", path: "posts/:id/edit", element: <EditPost /> },
-      { key:"11", path: "posts/:id/delete", element: <DeletePost /> },
-      { key:"12", path: "logout", element: <Logout /> },
-
+      { key: "1", path: "posts/:id", element: <PostDetail /> },
+      { key: "2", path: "register", element: <Register /> },
+      { key: "3", path: "login", element: <Login /> },
+      { key: "4", path: "profile/:id", element: <UserProfile /> },
+      { key: "5", path: "authors", element: <Authors /> },
+      { key: "6", path: "create", element: <CreatePost /> },
+      {
+        key: "7",
+        path: "posts/categories/:category",
+        element: <CategoryPosts />,
+      },
+      { key: "8", path: "posts/users/:id", element: <AuthorPosts /> },
+      { key: "9", path: "myposts/:id", element: <Dashboard /> },
+      { key: "10", path: "posts/:id/edit", element: <EditPost /> },
+      { key: "11", path: "posts/:id/delete", element: <DeletePost /> },
+      { key: "12", path: "logout", element: <Logout /> },
     ],
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const LayoutWithFonts = () => {
-  useEffect(() => {
-    WebFont.load({
-      google: {
-        families: ["Poppins:300,400,500,600,700"],
-      },
-    });
-  }, []);
-
-  return (
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
-};
-root.render(<LayoutWithFonts />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
